f3e0c8c08c5af8b24bfd9fae5b7e90c7f7a09711
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-13 04:03:35 +0900

    takenote.zsh: Add method (keep clean namespace)

--- takenote.zsh
+++ takenote.zsh
@@ -21,7 +21,7 @@ TAKENOTE_FILERCMD=${TAKENOTE_FILERCMD:-"xdg-open"}
 
 takenote() {
 
-  local dir daydir filename editor
+  local dir filename editor
   local filercmd=${TAKENOTE_FILERCMD}
   local rootdir=${TAKENOTE_ROOTDIR}
   local pattern='s/'"${TAKENOTE_FILENAME_PRE}"'\([0-9]\{'"${TAKENOTE_FILENAME_NUMORDER}"'\}\)'"${TAKENOTE_FILENAME_POST}"'.'"${TAKENOTE_FILENAME_EXTENSION}"'/\1/p'
@@ -48,10 +48,11 @@ takenote() {
   }
 
   takenote_init() {
+    local daydir
     # set the saving directory
     if [[ "${dir}" = "" ]]; then
       if check_dir "$rootdir"; then
-        local daydir="$(date +"${TAKENOTE_DAYDIR_FORMAT}")"
+        daydir="$(date +"${TAKENOTE_DAYDIR_FORMAT}")"
         dir="$rootdir/$daydir"
       else
         return 1
@@ -61,36 +62,14 @@ takenote() {
     fi
   }
 
-  takenote_showlist() {
-    takenote_init || return 1
-    # only show existing file in the directory
-    if check_dir "$dir"; then
-      list=$(ls "$dir")
-      echo "$list"
-    else
-      :
-    fi
-  }
-
-  takenote_openfiler() {
-    takenote_init || return 1
-    # move to today's directory by the user defined filer
-    if check_dir "$dir"; then
-      "$filercmd" "$dir"
-    else
-      echo "Open root directory '$rootdir' ..."
-      "$filercmd" "$rootdir"
-    fi
-  }
-
   takenote_main() {
-    takenote_init || return 1
+    takenote_init || (cleanup_namespace; return 1)
     # set the filename
     if [[ "${filename}" = "" ]]; then
       if [ -e "$dir" ]; then
-        i=$(( $(ls "$dir" | sed -n $pattern | tail -n 1) + 1 ))
+        local i=$(( $(ls "$dir" | sed -n $pattern | tail -n 1) + 1 ))
       else
-        i=1
+        local i=1
       fi
       filename="$(printf ${TAKENOTE_FILENAME_PRE}%0${TAKENOTE_FILENAME_NUMORDER}d.${TAKENOTE_FILENAME_EXTENSION} "$i")"
     else
@@ -139,6 +118,11 @@ takenote() {
     fi
   }
 
+  cleanup_namespace() {
+    unset -f show_usage check_dir takenote_init takenote_edit takenote_main
+    unset -f $0
+  }
+
   while getopts d:o:g:lrh OPT
   do
     case $OPT in
@@ -148,24 +132,44 @@ takenote() {
             ;;
       "g" ) editor="$OPTARG"
             ;;
-      "l" ) takenote_showlist
-            return 0
+      "l" ) # takenote_showlist
+            if takenote_init; then
+              # only show existing file in the directory
+              if check_dir "$dir"; then
+                local list=$(ls "$dir")
+                echo "$list"
+              fi
+              cleanup_namespace; return 0
+            else
+              cleanup_namespace; return 1
+            fi
             ;;
-      "r" ) takenote_openfiler
-            return 0
+      "r" ) # takenote_openfiler
+            if takenote_init; then
+              # move to today's directory by the user defined filer
+              if check_dir "$dir"; then
+                "$filercmd" "$dir"
+              else
+                echo "Open root directory '$rootdir' ..."
+                "$filercmd" "$rootdir"
+              fi
+              cleanup_namespace; return 0
+            else
+              cleanup_namespace; return 1
+            fi
             ;;
       "h" ) show_usage
-            return 0
+            cleanup_namespace; return 0
             ;;
         * ) show_usage
-            return 1
+            cleanup_namespace; return 1
             ;;
     esac
   done
 
   takenote_main
+  cleanup_namespace; return 0
 
-  return 0
 }
 
 _takenote() {
